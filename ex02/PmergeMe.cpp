79379bd6303f8eaddffa1411d54f9bfe9315f36b
yeongo <yeongo@student.42seoul.kr>
2023-11-23 21:02:39 +0900

    modify: set class PmergeMe

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,1 +1,59 @@
 #include "PmergeMe.hpp"
+#include <iostream>
+
+std::vector<int> PmergeMe::_vectorSequence;
+std::list<int> PmergeMe::_listSequence;
+double _vectorTime;
+double _listTime;
+
+void PmergeMe::sortVector() {
+	clock_t startTime = clock();
+	clock_t finishTime = clock();
+	_vectorTime = finishTime - startTime;
+}
+
+void PmergeMe::sortList() {
+	clock_t startTime = clock();
+	clock_t finishTime = clock();
+	_listTime = finishTime - startTime;
+
+}
+
+void PmergeMe::printUnsorted() {
+	std::cout << "Before:  ";
+	for (int i = 0; i < _vectorSequence.size(); i++) {
+		if (i != 0)
+			std::cout << " ";
+		std::cout << _vectorSequence[i];
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::printSorted() {
+	std::cout << "After:   ";
+	for (int i = 0; i < _vectorSequence.size(); i++) {
+		if (i != 0)
+			std::cout << " ";
+		std::cout << _vectorSequence[i];
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::printVectorTime() {
+	std::cout << "Time to process a range of " << _vectorSequence.size() <<
+	" elements with std::vector : " << _vectorTime << " us" << std::endl;
+}
+
+void PmergeMe::printListTime() {
+	std::cout << "Time to process a range of " << _listSequence.size() <<
+	" elements with std::list : " << _listTime << " us" << std::endl;
+}
+
+void PmergeMe::print() {
+	printUnsorted();
+	sortVector();
+	printSorted();
+	printVectorTime();
+	sortList();
+	printListTime();
+}
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -1,3 +1,32 @@
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
+
+#include <string>
+#include <list>
+#include <vector>
+#include <ctime>
+
+class PmergeMe {
+	public:
+		static void sortVector();
+		static void sortList();
+
+		static void printUnsorted();
+		static void printSorted();
+		static void printVectorTime();
+		static void printListTime();
+		static void print();
+
+	private:
+		static std::vector<int> _vectorSequence;
+		static std::list<int> _listSequence;
+		static double _vertorTime;
+		static double _listTime;
+
+		PmergeMe();
+		PmergeMe(PmergeMe const &object);
+		~PmergeMe();
+		PmergeMe &operator=(PmergeMe const &object);
+};
+
 #endif  // PMERGEME_HPP
